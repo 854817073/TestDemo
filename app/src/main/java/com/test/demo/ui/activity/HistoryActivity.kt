a2cb82a2295e17479b62fe7bbd9541b1cc3381c8
chenshuai <15023158107@qq.com>
2020-12-25 18:03:23 +0800

    add 注释

--- app/src/main/java/com/test/demo/ui/activity/HistoryActivity.kt
+++ app/src/main/java/com/test/demo/ui/activity/HistoryActivity.kt
@@ -2,7 +2,12 @@ package com.test.demo.ui.activity
 
 import com.test.demo.base.BaseActivity
 import com.test.demo.databinding.ActivityHistoryBinding
-
+/**
+ * @author This Man
+ * 版本：1.0
+ * 创建日期：2020-12-25
+ * 描述：
+ */
 class HistoryActivity : BaseActivity<ActivityHistoryBinding>() {
 
     override fun initData() {
